b18ac53f5ff3bd56ac568cebcb1804752551f516
Fernando Rueda <fernandoruedaoliva@gmail.com>
2017-10-20 15:20:40 +0200

    initial commit

---
+++ src/main/kotlin/com/fruedacode/learning/kotlinflux/KotlinFluxApplication.kt
@@ -0,0 +1,11 @@
+package com.fruedacode.learning.kotlinflux
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KotlinFluxApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(KotlinFluxApplication::class.java, *args)
+}
